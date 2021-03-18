423da5869dcb90143c5fc98587387aa60f6eb94f
huy2222001 <nguyentunghuy02@gmail.com>
2021-03-18 23:08:57 +0700

    line 13

--- OOP_19DTHA/src/TUAN03_Bai03/DanhSachCD.java
+++ OOP_19DTHA/src/TUAN03_Bai03/DanhSachCD.java
@@ -7,16 +7,22 @@ import java.util.Scanner;
 public class DanhSachCD {
 	CD[] dsCD;//= new CD[n]; 
 	DanhSachCD[] cd;
-	
-	
+	private int sl = 0;
+	public int getSl() {
+		return sl;
+	}
+	public void setSl(int sl) {
+		this.sl = sl;
+	}
 	public void NhapDSCD(int n) {
-		
+		dsCD = new CD[n];
 		for(int i = 0 ; i < n ; i++) {
+			
 			System.out.println("Nhap thong tin CD thu "+(i+1));
 			
-			cd[i]=new DanhSachCD();
+			//cd[i]=new DanhSachCD();
 			dsCD[i].Nhap1CD();
-			
+			sl++;
 		}
 	}
 	public void XuatDSCD() {
@@ -26,5 +32,6 @@ public class DanhSachCD {
 			dsCD[i].Xuat1CD();
 		}
 	}
+	
 }
 
